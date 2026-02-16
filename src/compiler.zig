408d01f67302a14273ff38a076878dfcbd1fb371
Dominik Heller <kilianmandscharo@gmail.com>
2026-02-16 22:04:36 +0100

    chapter 15: A Virtual Machine

--- src/compiler.zig
+++ src/compiler.zig
@@ -4,6 +4,11 @@ pub const OpCode = enum(u8) {
     Return,
     Constant,
     Constant_Long,
+    Negate,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
 };
 
 pub const OpByte = union(enum) {
@@ -40,6 +45,12 @@ pub const Chunk = struct {
 
     pub fn writeConstant(self: *Chunk, allocator: std.mem.Allocator, value: Value, line: usize) !void {
         const constant = try self.addConstant(allocator, value);
+        try self.write(allocator, OpByte{ .Op = .Constant }, line);
+        try self.write(allocator, OpByte{ .Byte = @intCast(constant) }, line);
+    }
+
+    pub fn writeConstantLong(self: *Chunk, allocator: std.mem.Allocator, value: Value, line: usize) !void {
+        const constant = try self.addConstant(allocator, value);
         try self.write(allocator, OpByte{ .Op = .Constant_Long }, line);
         try self.write(allocator, OpByte{ .Byte = @intCast((constant >> 16) & 0xFF) }, line);
         try self.write(allocator, OpByte{ .Byte = @intCast((constant >> 8) & 0xFF) }, line);
@@ -54,34 +65,156 @@ pub const Chunk = struct {
     pub fn disassemble(self: *Chunk) void {
         var offset: usize = 0;
         while (offset < self.code.items.len) {
-            std.debug.print("{d:0>4} ", .{offset});
-            if (offset > 0 and self.lines.items[offset] == self.lines.items[offset - 1]) {
-                std.debug.print("   | ", .{});
-            } else {
-                std.debug.print("{d:4} ", .{self.lines.items[offset]});
-            }
-            switch (@as(OpCode, @enumFromInt(self.code.items[offset]))) {
-                .Return => {
-                    std.debug.print("OP_RETURN\n", .{});
-                    offset += 1;
-                },
-                .Constant => {
-                    const constant = self.code.items[offset + 1];
-                    const value = self.constants.items[constant];
-                    std.debug.print("OP_CONSTANT {d:4} '{d}'\n", .{ constant, value });
-                    offset += 2;
-                },
-                .Constant_Long => {
-                    const b1 = self.code.items[offset + 1];
-                    const b2 = self.code.items[offset + 2];
-                    const b3 = self.code.items[offset + 3];
-                    const constant: usize = (@as(usize, @intCast(b1)) << 16) | (@as(usize, @intCast(b2)) << 8) | @as(usize, @intCast(b3));
-                    const value = self.constants.items[constant];
-                    std.debug.print("OP_CONSTANT_LONG {d:4} '{d}'\n", .{ constant, value });
-                    offset += 4;
-                },
-            }
+            offset = self.disassembleInstruction(offset);
         }
         std.debug.print("\n", .{});
+    }
+
+    fn simpleInstruction(name: []const u8, offset: usize) usize {
+        std.debug.print("{s}\n", .{name});
+        return offset + 1;
+    }
+
+    pub fn disassembleInstruction(self: *Chunk, offset: usize) usize {
+        std.debug.print("{d:0>4} ", .{offset});
+        if (offset > 0 and self.lines.items[offset] == self.lines.items[offset - 1]) {
+            std.debug.print("   | ", .{});
+        } else {
+            std.debug.print("{d:4} ", .{self.lines.items[offset]});
+        }
+        switch (@as(OpCode, @enumFromInt(self.code.items[offset]))) {
+            .Return => {
+                return Chunk.simpleInstruction("OP_RETURN", offset);
+            },
+            .Constant => {
+                const constant = self.code.items[offset + 1];
+                const value = self.constants.items[constant];
+                std.debug.print("OP_CONSTANT {d:4} '{d}'\n", .{ constant, value });
+                return offset + 2;
+            },
+            .Constant_Long => {
+                const b1 = self.code.items[offset + 1];
+                const b2 = self.code.items[offset + 2];
+                const b3 = self.code.items[offset + 3];
+                const constant: usize = (@as(usize, @intCast(b1)) << 16) | (@as(usize, @intCast(b2)) << 8) | @as(usize, @intCast(b3));
+                const value = self.constants.items[constant];
+                std.debug.print("OP_CONSTANT_LONG {d:4} '{d}'\n", .{ constant, value });
+                return offset + 4;
+            },
+            .Add => {
+                return Chunk.simpleInstruction("OP_ADD", offset);
+            },
+            .Subtract => {
+                return Chunk.simpleInstruction("OP_SUBTRACT", offset);
+            },
+            .Multiply => {
+                return Chunk.simpleInstruction("OP_MULTIPLY", offset);
+            },
+            .Divide => {
+                return Chunk.simpleInstruction("OP_DIVIDE", offset);
+            },
+            .Negate => {
+                return Chunk.simpleInstruction("OP_NEGATE", offset);
+            },
+        }
+    }
+};
+
+const DEBUG_TRACE_EXECUTION = true;
+const STACK_MAX = 256;
+
+const InterpretResult = enum {
+    Ok,
+    CompileError,
+    RuntimeError,
+};
+
+pub const VirtualMachine = struct {
+    chunk: *Chunk,
+    ip: usize,
+    stack: [STACK_MAX]Value,
+    stack_top: usize,
+
+    pub fn init() VirtualMachine {
+        return VirtualMachine{
+            .chunk = undefined,
+            .ip = 0,
+            .stack = undefined,
+            .stack_top = 0,
+        };
+    }
+
+    fn resetStack(self: *VirtualMachine) void {
+        self.stack_top = 0;
+    }
+
+    fn push(self: *VirtualMachine, value: Value) void {
+        self.stack[self.stack_top] = value;
+        self.stack_top += 1;
+    }
+
+    fn pop(self: *VirtualMachine) Value {
+        self.stack_top -= 1;
+        return self.stack[self.stack_top];
+    }
+
+    pub fn interpret(self: *VirtualMachine, chunk: *Chunk) InterpretResult {
+        self.chunk = chunk;
+        return self.run();
+    }
+
+    pub inline fn readByte(self: *VirtualMachine) u8 {
+        const instruction = self.chunk.code.items[self.ip];
+        self.ip += 1;
+        return instruction;
+    }
+
+    pub fn run(self: *VirtualMachine) InterpretResult {
+        while (true) {
+            if (comptime DEBUG_TRACE_EXECUTION) {
+                std.debug.print("          ", .{});
+                for (0..self.stack_top) |index| {
+                    std.debug.print("[ {d} ]", .{self.stack[index]});
+                }
+                std.debug.print("\n", .{});
+                _ = self.chunk.disassembleInstruction(self.ip);
+            }
+            const instruction = self.readByte();
+            switch (@as(OpCode, @enumFromInt(instruction))) {
+                .Constant => {
+                    const constant = self.chunk.constants.items[self.readByte()];
+                    self.push(constant);
+                },
+                .Add => {
+                    const b = self.pop();
+                    const a = self.pop();
+                    self.push(a + b);
+                },
+                .Subtract => {
+                    const b = self.pop();
+                    const a = self.pop();
+                    self.push(a - b);
+                },
+                .Multiply => {
+                    const b = self.pop();
+                    const a = self.pop();
+                    self.push(a * b);
+                },
+                .Divide => {
+                    const b = self.pop();
+                    const a = self.pop();
+                    self.push(a / b);
+                },
+                .Negate => {
+                    self.push(-self.pop());
+                },
+                .Return => {
+                    const value = self.pop();
+                    std.debug.print("{d}\n", .{value});
+                    return .Ok;
+                },
+                else => return .RuntimeError,
+            }
+        }
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -2,16 +2,27 @@ const std = @import("std");
 const compiler = @import("compiler.zig");
 const Value = compiler.Value;
 const OpByte = compiler.OpByte;
+const VirtualMachine = compiler.VirtualMachine;
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
+    var vm = VirtualMachine.init();
+
     const allocator = arena.allocator();
 
     var chunk = compiler.Chunk.init();
+
     try chunk.writeConstant(allocator, 1.2, 123);
-    try chunk.writeConstant(allocator, 1.2, 123);
-    try chunk.writeConstant(allocator, 1.2, 123);
-    chunk.disassemble();
+    try chunk.writeConstant(allocator, 3.4, 123);
+    try chunk.write(allocator, OpByte{ .Op = .Add }, 123);
+
+    try chunk.writeConstant(allocator, 5.6, 123);
+    try chunk.write(allocator, OpByte{ .Op = .Divide }, 123);
+
+    try chunk.write(allocator, OpByte{ .Op = .Negate }, 123);
+    try chunk.write(allocator, OpByte{ .Op = .Return }, 123);
+
+    _ = vm.interpret(&chunk);
 }
